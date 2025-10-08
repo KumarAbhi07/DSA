ded9fc9a0c4f324effd50fb86a490136201ec216
Abhimanyu Kumar <abhimanyukumar072003@gmail.com>
2025-10-09 05:09:03 +0530

    new file is added

--- LinkedlistPart1/LinkedList.java
+++ LinkedlistPart1/LinkedList.java
@@ -16,21 +16,52 @@ public class LinkedList {
     public void addFirst(int data){
         Node newNode=new Node(data);
         if(head==null) {
-            head=tail=null;
+            head=tail=newNode;
+            return;
         }
 
         newNode.next=head;
         head=newNode;
     }
     // add last
-    public void addLast(in)
+    public void addLast(int data){
+        Node newNode=new Node(data);
+        if(head==null){
+            head=tail=newNode;
+            return;
+        }
+        tail.next=newNode;
+        tail=newNode;
+
+    }
+    //print linkedlist
+    public void print(){
+        if(head==null){
+            System.out.println("ll is empty");
+            return;
+        }
+        Node temp=head;
+        while(temp!=null){
+            System.out.print(temp.data+"->");
+            temp=temp.next;
+        }
+        System.out.println("null");
+    }
 
 
     public static void main(String[] args) {
         LinkedList ll=new LinkedList();
+        ll.print();
         ll.addFirst(1);
+        ll.print();
         ll.addFirst(34);
+        ll.print();
+        ll.addLast(5);
+        ll.addLast(45);
+        ll.print();
+
 
 
     }
 }
+
