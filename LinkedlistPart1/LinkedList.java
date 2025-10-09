2b1e8d34a438d76ce31f8807b03970629398216a
Abhimanyu Kumar <abhimanyukumar072003@gmail.com>
2025-10-09 06:07:58 +0530

    linkedlsi basic is completed

--- LinkedlistPart1/LinkedList.java
+++ LinkedlistPart1/LinkedList.java
@@ -11,10 +11,12 @@ public class LinkedList {
     }
     public static Node head;
     public static Node tail;
+    public static int size;
      // add element to first
 
     public void addFirst(int data){
         Node newNode=new Node(data);
+        size++;
         if(head==null) {
             head=tail=newNode;
             return;
@@ -26,6 +28,7 @@ public class LinkedList {
     // add last
     public void addLast(int data){
         Node newNode=new Node(data);
+        size++;
         if(head==null){
             head=tail=newNode;
             return;
@@ -47,6 +50,61 @@ public class LinkedList {
         }
         System.out.println("null");
     }
+    // add in particular index;
+    public void add(int idx, int data){
+        if(idx==0){
+            addFirst(data);
+            return;
+        }
+        Node newNode=new Node(data);
+
+        size++;
+        Node temp=head;
+        int i=0;
+        while(i<idx-1){
+            temp=temp.next;
+            i++;
+        }
+        newNode.next=temp.next;
+        temp.next=newNode;
+    }
+    public int removeFirst(){
+        if(size==0){
+            System.out.println("ll is empty");
+            return Integer.MIN_VALUE;
+        }else if(size==1){
+            int val=head.data;
+            size=0;
+            head=tail=null;
+            return val;
+
+        }
+        int val=head.data;
+        head=head.next;
+        size--;
+        return val;
+    }
+    public int removeLast(){
+        if(size==0){
+            System.out.println("LL is empty");
+            return Integer.MIN_VALUE;
+        }else if(size==1){
+            int val=head.data;
+            head=tail=null;
+            size=0;
+            return val;
+        }
+        Node prev=head;
+        for(int i=0;i<size-2;i++){
+            prev=prev.next;
+
+        }
+        int val=prev.next.data;
+        prev.next=null;
+        tail=prev;
+        size--;
+        return val;
+    }
 
 
     public static void main(String[] args) {
@@ -58,6 +116,12 @@ public class LinkedList {
         ll.print();
         ll.addLast(5);
         ll.addLast(45);
+        ll.print();
+        ll.add(2,56);
+        ll.print();
+        ll.removeFirst();
+        ll.print();
+        ll.removeLast();
         ll.print();
 
 
